d79c5cb3428b94ce7cb07e415741ccabcb7e97ef
winzfs <jazzhjm@gmail.com>
2026-05-23 22:46:18 +0900

    Register question scene and DOM support

--- src/game/GameConfig.ts
+++ src/game/GameConfig.ts
@@ -1,6 +1,7 @@
 import Phaser from "phaser";
 import { BootScene } from "./scenes/BootScene";
 import { IntroScene } from "./scenes/IntroScene";
+import { QuestionScene } from "./scenes/QuestionScene";
 
 export const GAME_WIDTH = 390;
 export const GAME_HEIGHT = 844;
@@ -15,10 +16,13 @@ export const gameConfig: Phaser.Types.Core.GameConfig = {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
+  dom: {
+    createContainer: true,
+  },
   render: {
     antialias: true,
     pixelArt: false,
     roundPixels: false,
   },
-  scene: [BootScene, IntroScene],
+  scene: [BootScene, IntroScene, QuestionScene],
 };
