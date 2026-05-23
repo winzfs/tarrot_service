df383488a36aef82651e6aff5bae33ab2b8c2cf7
winzfs <jazzhjm@gmail.com>
2026-05-24 03:07:03 +0900

    Load tarot card image assets

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -1,8 +1,15 @@
 import Phaser from "phaser";
+import { allTarotCards } from "../../tarot/cards";
 
 export class BootScene extends Phaser.Scene {
   constructor() {
     super("BootScene");
+  }
+
+  preload(): void {
+    for (const card of allTarotCards) {
+      this.load.image(card.imageKey, card.imageUrl);
+    }
   }
 
   create(): void {
