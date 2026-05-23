a1107b47f4ba3737afe3ffd3702134020f704e31
winzfs <jazzhjm@gmail.com>
2026-05-23 22:42:34 +0900

    Add Phaser game configuration

---
+++ src/game/GameConfig.ts
@@ -0,0 +1,24 @@
+import Phaser from "phaser";
+import { BootScene } from "./scenes/BootScene";
+import { IntroScene } from "./scenes/IntroScene";
+
+export const GAME_WIDTH = 390;
+export const GAME_HEIGHT = 844;
+
+export const gameConfig: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: "game-root",
+  backgroundColor: "#09071a",
+  width: GAME_WIDTH,
+  height: GAME_HEIGHT,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  render: {
+    antialias: true,
+    pixelArt: false,
+    roundPixels: false,
+  },
+  scene: [BootScene, IntroScene],
+};
