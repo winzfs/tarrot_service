a15668867552783eceaf51c0ebf0796125a0c9ff
winzfs <jazzhjm@gmail.com>
2026-05-25 02:51:15 +0900

    Preload VFX assets needed by intro scene

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import cardBackImageUrl from "../../../images/back.png?url";
+import { allVfxAssets } from "../vfx/vfxLibrary";
 
 export const CARD_BACK_IMAGE_KEY = "tarot-card-back";
 export const INTRO_TITLE_IMAGE_KEY = "intro-title-image";
@@ -12,6 +13,11 @@ export class BootScene extends Phaser.Scene {
   preload(): void {
     this.load.image(CARD_BACK_IMAGE_KEY, cardBackImageUrl);
     this.load.image(INTRO_TITLE_IMAGE_KEY, "/img/title.png");
+    allVfxAssets.forEach((asset) => {
+      if (!this.textures.exists(asset.key)) {
+        this.load.image(asset.key, asset.url);
+      }
+    });
   }
 
   create(): void {
