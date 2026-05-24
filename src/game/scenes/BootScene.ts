fd73818c36d00b26e59c11ee8993630b6bfbc9d8
winzfs <jazzhjm@gmail.com>
2026-05-25 02:01:43 +0900

    Load only critical intro assets in boot

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -1,6 +1,4 @@
 import Phaser from "phaser";
-import { allTarotCards } from "../../tarot/cards";
-import { allVfxAssets } from "../vfx/vfxLibrary";
 import cardBackImageUrl from "../../../images/back.png?url";
 
 export const CARD_BACK_IMAGE_KEY = "tarot-card-back";
@@ -14,14 +12,6 @@ export class BootScene extends Phaser.Scene {
   preload(): void {
     this.load.image(CARD_BACK_IMAGE_KEY, cardBackImageUrl);
     this.load.image(INTRO_TITLE_IMAGE_KEY, "/img/title.png");
-
-    for (const card of allTarotCards) {
-      this.load.image(card.imageKey, card.imageUrl);
-    }
-
-    for (const asset of allVfxAssets) {
-      this.load.image(asset.key, asset.url);
-    }
   }
 
   create(): void {
