b6d08c14048c6228fb70f27ec67a2b9433d483f3
winzfs <jazzhjm@gmail.com>
2026-05-25 01:06:49 +0900

    feat: preload intro title image

--- src/game/scenes/BootScene.ts
+++ src/game/scenes/BootScene.ts
@@ -4,6 +4,7 @@ import { allVfxAssets } from "../vfx/vfxLibrary";
 import cardBackImageUrl from "../../../images/back.png?url";
 
 export const CARD_BACK_IMAGE_KEY = "tarot-card-back";
+export const INTRO_TITLE_IMAGE_KEY = "intro-title-image";
 
 export class BootScene extends Phaser.Scene {
   constructor() {
@@ -12,6 +13,7 @@ export class BootScene extends Phaser.Scene {
 
   preload(): void {
     this.load.image(CARD_BACK_IMAGE_KEY, cardBackImageUrl);
+    this.load.image(INTRO_TITLE_IMAGE_KEY, "/img/tltle.png");
 
     for (const card of allTarotCards) {
       this.load.image(card.imageKey, card.imageUrl);
