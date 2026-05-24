50ad828f25a3f6347f46806ed0eee16421049066
winzfs <jazzhjm@gmail.com>
2026-05-25 02:55:39 +0900

    Bypass internal card select shuffle to restore scene progression

--- src/game/patches/cardSelectCameraFadePatch.ts
+++ src/game/patches/cardSelectCameraFadePatch.ts
@@ -1,20 +1,19 @@
 import { CardSelectScene } from "../scenes/CardSelectScene";
 
 type PatchedCardSelectScene = CardSelectScene & {
+  create: (...args: unknown[]) => unknown;
   createCardSelectionStage: (...args: unknown[]) => unknown;
-  __cardSelectCameraFadePatchApplied?: boolean;
+  __cardSelectProgressionPatchApplied?: boolean;
 };
 
 const prototype = CardSelectScene.prototype as unknown as PatchedCardSelectScene;
 
-if (!prototype.__cardSelectCameraFadePatchApplied && typeof prototype.createCardSelectionStage === "function") {
-  const originalCreateCardSelectionStage = prototype.createCardSelectionStage;
-
-  prototype.createCardSelectionStage = function patchedCreateCardSelectionStage(this: CardSelectScene, ...args: unknown[]) {
-    const result = originalCreateCardSelectionStage.apply(this, args);
+if (!prototype.__cardSelectProgressionPatchApplied && typeof prototype.createCardSelectionStage === "function") {
+  prototype.create = function patchedCreate(this: CardSelectScene) {
+    this.cameras.main.resetFX();
     this.cameras.main.fadeIn(260, 9, 7, 26);
-    return result;
+    return (this as unknown as PatchedCardSelectScene).createCardSelectionStage();
   };
 
-  prototype.__cardSelectCameraFadePatchApplied = true;
+  prototype.__cardSelectProgressionPatchApplied = true;
 }
